97d85b20fe08dc290b262c6c34f0ff803220ea7c
damjanh <damjan.hudobreznik@gmail.com>
2023-12-27 12:49:37 +0100

    Add system ordering for player movement and player confinment.

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -9,8 +9,7 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(spawn_player)
-            .add_system(player_movement)
-            .add_system(confine_player_movement)
+            .add_systems((player_movement, confine_player_movement).chain())
             .add_system(enemy_player_collision)
             .add_system(pickup_player_collision);
     }
