3826a2d5fd17108f67d8fefb0859b899c9b01777
damjanh <damjan.hudobreznik@gmail.com>
2023-12-27 16:14:19 +0100

    Prepare systems for changing simulation state and app state.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -2,24 +2,36 @@ use bevy::prelude::*;
 
 mod pickup;
 mod player;
+mod systems;
 
 pub mod enemy;
 pub mod score;
 
 use crate::events::GameOver;
+use crate::AppState;
 
 use enemy::EnemyPlugin;
 use pickup::PickupPlugin;
 use player::PlayerPlugin;
 use score::ScorePlugin;
+use systems::*;
 
 pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_event::<GameOver>()
+        app.add_state::<SimulationState>()
+            .add_event::<GameOver>()
             .add_plugin(EnemyPlugin)
             .add_plugin(PlayerPlugin)
             .add_plugin(ScorePlugin)
-            .add_plugin(PickupPlugin);
+            .add_plugin(PickupPlugin)
+            .add_system(toggle_simulation.run_if(in_state(AppState::Game)));
     }
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum SimulationState {
+    Running,
+    #[default]
+    Paused,
 }
--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,21 @@ use systems::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_state::<AppState>()
         .add_plugin(GamePlugin)
         .add_plugin(MenuPlugin)
         .add_startup_system(spawn_camera)
         .add_system(exit_game)
         .add_system(handle_game_over)
+        .add_system(transition_to_game_state)
+        .add_system(transition_to_main_menu_state)
         .run();
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    #[default]
+    MainMenu,
+    Game,
+    GameOver,
 }
